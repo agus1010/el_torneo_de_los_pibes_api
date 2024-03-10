8b054fd87ecdefd29dffa5a5b1a93aac4fb2c27d
Agus <agus.aguirre10@gmail.com>
2024-03-09 22:34:10 -0300

    Matches controller done

--- api/Controllers/MatchesController.cs
+++ api/Controllers/MatchesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
 using api.Models.Dtos.Match;
+using api.Services;
+using api.Services.Errors;
 
 
 namespace api.Controllers
@@ -9,28 +11,77 @@ namespace api.Controllers
     [ApiController]
     public class MatchesController : ControllerBase
     {
-        [HttpPost]
-		[ProducesResponseType(StatusCodes.Status200OK)]
-		[ProducesResponseType(StatusCodes.Status400BadRequest)]
-		[ProducesResponseType(StatusCodes.Status404NotFound)]
-		public async Task<ActionResult> CreateMatch(PlayersOnlyFriendlyMatchCreationDto matchCreationDto)
+        protected readonly MatchesService _matchesService;
+        public MatchesController(MatchesService matchesService)
         {
-            throw new NotImplementedException();
+            _matchesService = matchesService;
+        }
+
+
+        [HttpPost]
+		[ProducesResponseType(StatusCodes.Status201Created)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		public async Task<ActionResult<MatchDto>> CreateMatch(PlayersOnlyFriendlyMatchCreationDto matchCreationDto)
+        {
+            if (matchCreationDto.Team1Dto == null || matchCreationDto.Team2Dto == null)
+                return BadRequest();
+            var createdMatch = await _matchesService.CreateAsync(matchCreationDto);
+            return CreatedAtAction("GetMatch", new { id = createdMatch.Id }, createdMatch);
         }
 
 
 		[HttpGet("{id}")]
-        public async Task<ActionResult<MatchDto>> GetMatch(int id)
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public async Task<ActionResult<MatchDto>> GetMatch(int id)
         {
-            throw new NotImplementedException();
+            if (id <= 0)
+                return BadRequest();
+            var matchDto = await _matchesService.GetAsync(id);
+            if (matchDto == null)
+                return NotFound();
+            return Ok(matchDto);
         }
 
 
         [HttpPut("{id}")]
-        
-        public async Task<ActionResult> EditMatch(int id, MatchUpdateDto matchUpdateDto)
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public async Task<ActionResult> EditMatch(int id, MatchUpdateDto matchUpdateDto)
         {
-            throw new NotImplementedException();
-        }
-    }
+			if (id <= 0)
+				return BadRequest();
+			try
+			{
+				await _matchesService.UpdateAsync(id, matchUpdateDto);
+			}
+			catch (EntityNotFoundException)
+			{
+				return NotFound();
+			}
+			return NoContent();
+		}
+
+
+		[HttpPut("{id}")]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public async Task<ActionResult> DeleteMatch(int id, MatchUpdateDto matchUpdateDto)
+		{
+			if (id <= 0)
+				return BadRequest();
+			try
+			{
+				await _matchesService.DeleteAsync(id);
+			}
+			catch (EntityNotFoundException)
+			{
+				return NotFound();
+			}
+			return NoContent();
+		}
+	}
 }
--- api/Services/MatchesService.cs
+++ api/Services/MatchesService.cs
@@ -1,6 +1,43 @@
-﻿namespace api.Services
+﻿using api.Models.Dtos.Match;
+using api.Repositories;
+using AutoMapper;
+
+namespace api.Services
 {
 	public class MatchesService
 	{
+		protected readonly MatchesRepository matchesRepository;
+		protected readonly PlayersService playersService;
+		protected readonly IMapper mapper;
+
+		public MatchesService(MatchesRepository matchesRepository, PlayersService playersService, IMapper mapper)
+		{
+			this.matchesRepository = matchesRepository;
+			this.playersService = playersService;
+			this.mapper = mapper;
+		}
+
+		public async Task<MatchDto> CreateAsync(PlayersOnlyFriendlyMatchCreationDto matchCreationDto)
+		{
+			throw new NotImplementedException();
+		}
+
+		
+		public async Task<MatchDto?> GetAsync(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+
+		public async Task UpdateAsync(int id, MatchUpdateDto matchUpdateDto)
+		{
+			throw new NotImplementedException();
+		}
+
+
+		public async Task DeleteAsync(int id)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
